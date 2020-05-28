b457dc72fa492f570f18f48f9a8d6a610a7d1a87
tiagofrioli <tiago.frioli@gmail.com>
2020-05-28 00:50:58 -0300

    Estrutura das Páginas - Usabilidade do Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 import { Container } from './styles';
@@ -9,8 +15,26 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocus, setIsFocus] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
   const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocus(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocus(false);
+
+    // if (inputRef.current?.value) {
+    //   setIsFilled(true);
+    // } else {
+    //   setIsFilled(false);
+    // } ou
+
+    setIsFilled(!!inputRef.current?.value); // converte em booleano
+  }, []);
 
   useEffect(() => {
     registerField({
@@ -20,9 +44,15 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     });
   }, [fieldName, registerField]);
   return (
-    <Container>
+    <Container isFilled={isFilled} isFocus={isFocus}>
       {Icon && <Icon size={20} />}
-      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+      <input
+        onBlur={handleInputBlur}
+        onFocus={handleInputFocus}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
     </Container>
   );
 };
